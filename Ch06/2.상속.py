3788759d418ef7577d4ffe67b649a06bc2c56809
seonjuHa <gktjswn15@gmail.com>
2023-01-11 19:40:58 +0900

    2023/01/11

---
+++ Ch06/2.상속.py
@@ -0,0 +1,10 @@
+"""
+
+"""
+
+from sub1.StockAccount import StockAccount
+
+kb = StockAccount('KB증권', '101-12-1001', '홍길동', 50000, '삼성전자', 10, 60000)
+kb.deposit(1000000)
+kb.sell(5, 65000)
+kb.show()
---
+++ Ch06/sub2/calc.py
@@ -0,0 +1,16 @@
+# 파일이름이 소문자로 시작하여, 클래스는 아님
+
+def plus(x, y):
+    return x + y
+
+def minus(x, y):
+    return x - y
+
+def multi(x, y):
+    return x * y
+
+def div(x, y):
+    return x / y
+
+if __name__ == '__main__':
+    print('calc 실행...')
